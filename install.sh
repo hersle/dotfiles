ea2f6de369eef8439b675c448731284ae57acacb
Herman Sletmoen <hermansletmoen@gmail.com>
2017-12-05 20:18:00 +0100

    Improved installation script

--- install.sh
+++ install.sh
@@ -1,32 +1,21 @@
 #!/bin/sh
 
-if [ $# -ne 1 ]; then
-	echo "error: installation path not specified"
-	exit 1
-fi
+# assumes no paths contain spaces!
 
-basepath=$1
+cd $(dirname $0)/dotfiles
 
-for file in $(find dotfiles/ -type f); do
-	path="$basepath/$(echo $file | cut -d/ -f2-)"
-	file="$PWD/$file"
+for src in $(find -type f -printf '%P\n'); do
+	dst=$HOME/$src
+	dir=$(dirname $dst)
 
-	ans="y"
-	if [ -e $path ]; then
-		echo -n "overwrite $path? "
-		read ans
-		if [ "$ans" = "y" ]; then
-			rm $path
-		fi
+	if [ ! -d $dir ]; then
+		mkdir -p $dir
+		echo "created directory $dir"
+	elif [ -e $dst ] && ! cmp -s $src $dst; then
+		echo -n "overwrite $dst? "
+		read ans && [ "$ans" != "y" ] && continue
 	fi
 
-	if [ "$ans" = "y" ]; then
-		dir="$(dirname $path)"
-		if [ ! -d $dir ]; then
-			mkdir -p $dir
-			echo "created directory $dir"
-		fi
-		ln -sf $file $path
-		echo "installed $path"
-	fi
+	ln -sf $PWD/$src $dst
+	echo "installed $dst"
 done
